15942341f3170a5c08eb748fe6d199b260672851
kazzastic <rkazim79@gmail.com>
2019-03-04 04:15:31 +0500

    the app works offline now! :)

--- work/scripts/service-worker.js
+++ work/scripts/service-worker.js
@@ -1,5 +1,24 @@
 var cacheName = 'weatherPWA-step-6-1';
-var filesToCache = [];
+//var filesToCache = [];
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/app.js',
+  '/styles/inline.css',
+  '/images/clear.png',
+  '/images/cloudy-scattered-showers.png',
+  '/images/cloudy.png',
+  '/images/fog.png',
+  '/images/ic_add_white_24px.svg',
+  '/images/ic_refresh_white_24px.svg',
+  '/images/partly-cloudy.png',
+  '/images/rain.png',
+  '/images/scattered-showers.png',
+  '/images/sleet.png',
+  '/images/snow.png',
+  '/images/thunderstorm.png',
+  '/images/wind.png'
+];
 
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
@@ -13,4 +32,22 @@ self.addEventListener('install', function(e) {
 
 self.addEventListener('activate', function(e){
 	console.log('[ServiceWorker] Activate');
+	e.waitUntil(
+		caches.keys().then(function(keyList){
+			if(key !== cacheName){
+				console.log('[ServiceWorker] Removing old cache', key);
+				return caches.delete(key);
+			}
+		})
+		);
+	return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e){
+	console.log('[ServiceWorker] Fetch', e.request.url);
+	e.respondWith(
+		caches.match(e.request).then(function(response){
+			return response || fetch(e.request);
+		})
+		);
 });
