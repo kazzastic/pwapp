8bd19f8c01e09f72bfc2c74ad3445508970fc681
kazzastic <rkazim79@gmail.com>
2019-03-04 03:47:15 +0500

    service worker.js added

--- work/scripts/app.js
+++ work/scripts/app.js
@@ -330,4 +330,7 @@
   }
 
   // TODO add service worker code here
+  if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('./service-worker.js').then(function(){console.log('Service Worker Registered');});
+  }
 })();
